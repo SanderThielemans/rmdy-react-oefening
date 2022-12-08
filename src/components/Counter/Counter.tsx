5c8b819f35d81c7d3239c032d39f204417d99118
Sander Thielemans <thielemans.sander@gmail.com>
2022-12-08 14:37:40 +0100

    1. Counter test

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -5,8 +5,17 @@ import { Container, Controls, Reset, CustomInput } from './Counter.styles';
 const intialCount = 12;
 
 const Counter: React.FC = () => {
+    const [count, setCount] = React.useState<number>(intialCount);
+    const [customAdder, setCustomAdder] = React.useState<number>(5);
 
-    const [count, setCount] = React.useState<number>(intialCount)
+    const addNumber = (adder: number) => {
+        const result = count + adder;
+        setCount(result % 11 === 0 ? 0 : result);
+    }
+
+    const resetNumber = () => {
+        setCount(intialCount);
+    }
 
     return (
         <Container>
@@ -14,15 +23,15 @@ const Counter: React.FC = () => {
                 <p>{count}</p>
             </div>
             <Controls>
-                <button> -5 </button>
-                <button> -1 </button>
-                <button> +1 </button>
-                <button> +5 </button>
+                <button onClick={e => addNumber(-5)}> -5 </button>
+                <button onClick={e => addNumber(-1)}> -1 </button>
+                <button onClick={e => addNumber(1)}> +1 </button>
+                <button onClick={e => addNumber(5)}> +5 </button>
             </Controls>
-            <Reset>RESET</Reset>
+            <Reset onClick={e => resetNumber()}> RESET</Reset>
             <CustomInput>
-                <input type='number'/>
-                <button>ADD</button>
+                <input type='number' value={customAdder} onChange={e => setCustomAdder(Number(e.target.value))} />
+                <button onClick={e => addNumber(customAdder)}>ADD</button>
             </CustomInput>
         </Container>
     )
